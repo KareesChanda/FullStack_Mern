9526a8a49c085ae2b7369fc9789aa7db592c698e
mwamba <mwambachanda51@gmail.com>
2025-02-02 02:15:45 -0500

    In this Commit, I created authentication controller for sign-up and sign-in, created JWT and middleware. Handled errors and also delt with the cors error by creating a proxy to handle http request between front and backend.

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,7 +1,9 @@
 import User from "../models/user.model.js"
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/errors.js";
+import jwt from "jsonwebtoken";
 
+//this is a controller function to handle signing Up
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
 
@@ -16,4 +18,27 @@ export const signup = async (req, res, next) => {
     } catch (error) {
         next(error); //also can use errorHandler that i created here to create personalized errors
     }
-}
+};
+
+//this is a controller function to handle signing In
+export const signin = async (req, res, next) => {
+
+    const { email, password } = req.body;
+    
+    //use above email and password to authenticate in the below try-catch
+    try {
+        const validUser = await User.findOne({email});
+        if(!validUser) return next(errorHandler(404, "User not found"));
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword) return next(errorHandler(401, "Invalid Credentials"));
+
+        //I am creating a Json Web Token here (JWT) that allows a user's previously cached login to be remembered by the client-side
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+        res.cookie('access_Token', token, {httpOnly: true}).status(200).json({message: "user logged in successfully"});
+
+    } catch (error) {
+        next(error);
+    }
+
+};
